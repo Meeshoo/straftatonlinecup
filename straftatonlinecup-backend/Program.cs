3d116ac6ab4f115a55a0043b6234ee1f4cac453d
Meeshoo <mitch1080@hotmail.co.uk>
2024-09-22 14:25:54 +0100

    Add BASE_URL to debug

--- straftatonlinecup-backend/Program.cs
+++ straftatonlinecup-backend/Program.cs
@@ -106,7 +106,7 @@ app.MapGet("/debug", async (context) => {
     string? steamNickname = user.Identity.Name;
 
     if (user.Identity.IsAuthenticated) {
-        await context.Response.WriteAsync($"Welcome, {steamNickname}! Your Steam ID is {steamId}");
+        await context.Response.WriteAsync($"Welcome, {steamNickname}! Your Steam ID is {steamId}. Base URL is {BASE_URL}");
     } else {
         await context.Response.WriteAsync("You are not yet logged tf in");
     }
